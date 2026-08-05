ff3613a78bea2aca8b103ee600503c5dc0922392
Elián Fabián <elian.fabian.2001@gmail.com>
2026-08-05 14:19:21 +0200

    Remove LazyDescriptorsMode enum

--- kproxyable-runtime/src/commonMain/kotlin/com/elianfabian/kproxyable/KProxyable.kt
+++ kproxyable-runtime/src/commonMain/kotlin/com/elianfabian/kproxyable/KProxyable.kt
@@ -8,33 +8,10 @@ package com.elianfabian.kproxyable
  *
  * Can also be used as a **meta-annotation** on custom annotations (e.g., `@HttpClient`) to automatically
  * trigger proxy generation for any interface annotated with those custom annotations.
- *
- * @property lazyDescriptors Defines the initialization behavior for function and property descriptors.
- * Defaults to [LazyDescriptorsMode.AUTO].
  */
 @Target(
 	AnnotationTarget.CLASS,
 	AnnotationTarget.ANNOTATION_CLASS,
 )
 @Retention(AnnotationRetention.BINARY)
-public annotation class KProxyable(
-	val lazyDescriptors: LazyDescriptorsMode = LazyDescriptorsMode.AUTO
-)
-
-public enum class LazyDescriptorsMode {
-
-	/**
-	 * KSP decides based on member count (>= 20 members -> lazy).
-	 */
-	AUTO,
-
-	/**
-	 * Always generate 'by lazy'.
-	 */
-	ALWAYS,
-
-	/**
-	 * Always generate direct eager properties.
-	 */
-	NEVER,
-}
+public annotation class KProxyable
